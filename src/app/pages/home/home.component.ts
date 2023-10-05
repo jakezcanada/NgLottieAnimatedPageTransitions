57600afcae97d1a6957bffe76eef44fb61edd19e
Jake <jakezcanada@gmail.com>
2023-10-05 09:43:18 -0400

    Add home details

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor() { }
+  ngOnInit(): void {
+    const target = document.querySelector('.tw')
 
+    const options = {
+      loop: true
+    }
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 100,
+      deleteSpeed: 20,
+      typeColor: 'red'
+    })
+
+    writer
+      .type('Web Development')
+      .rest(500)
+      .remove(11)
+      .type('Design')
+      .rest(500)
+      .clear()
+      .type('Frontend Development')
+      .rest(500)
+      .clear()
+      .type('Backend Development')
+      .rest(500)
+      .clear()
+      .start()
+  }
 }
